bfa8da7cfdc59c6aa9c560064fc7562092341f15
Daniel Ransdorf <daniel.ransdorf@proton.me>
2025-03-26 16:53:04 +0100

    Nether 0.2.6 - update message created_at

--- src/nether/__init__.py
+++ src/nether/__init__.py
@@ -1,3 +1,3 @@
 from .application import Application as Application
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
--- src/nether/common.py
+++ src/nether/common.py
@@ -1,7 +1,7 @@
-import time
 from collections.abc import AsyncIterator, Iterator
 from contextlib import asynccontextmanager, contextmanager
 from dataclasses import dataclass, field
+from datetime import UTC, datetime
 from typing import Protocol
 
 
@@ -13,7 +13,7 @@ class ServiceError(Exception): ...
 
 @dataclass(frozen=True, kw_only=True)
 class Message:
-  created_at: float = field(default_factory=time.time)
+  created_at: datetime = field(default_factory=lambda: datetime.now(tz=UTC))
   ...
 
 
